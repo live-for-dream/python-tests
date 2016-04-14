a056b3c2e8aabdf25f4e0aba85ddce4e76f5c48f
centos64 <macg@fastweb.com.cn>
2016-04-14 18:08:25 +0800

    python-core-pragram

---
+++ hello-world/test1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#a first script file
+import sys
+
+print(sys.platform)
+
+print(2 ** 100)
+
+spam = 'Hello world! '
+print(spam * 8)
---
+++ python-core-pragram/ch03/MakeTextFile.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+'create a text file and input some data'
+
+import pdb
+import os
+
+#pdb.set_trace() 
+def input_filename():
+    'Let user input a filename which not exsists'
+    while True:
+        name = raw_input('file-name:\n>')
+        if os.path.exists(name):
+            print "*** ERROR: '%s' already exists" % name
+        else:
+            return name
+
+def input_data():
+    'get user input util . charactor'
+    all = []
+    while True:
+        entry = raw_input('> ')
+        if entry == '.':
+            return all
+        else:
+            all.append(entry)
+           
+def main():
+    'main function'
+
+    pdb.set_trace()    
+    print('START:')
+    fname = input_filename()
+    fdata = input_data()
+    
+    fobj = open(fname, 'w')
+    fobj.write('\n'.join(fdata))
+    fobj.close()
+    print('DONE')
+if __name__ == '__main__':    
+    pdb.set_trace()  
+    main()  
